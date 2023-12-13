c8fb4b5118955e5ef88db3fa210362f9b8d84e0a
ART0015058 <ART0015058@aluno.firjansenaisesi.com.br>
2023-12-13 12:38:22 -0300

    formatação css iniciada

--- games-api/js/main.js
+++ games-api/js/main.js
@@ -9,9 +9,16 @@ const loadGames = () => {
   console.log(">>>");
   const dataContainer = document.getElementById("data-container");
   getAllGames().then((resp) => {
-    resp.forEach((game) => {
+    resp.forEach((jogo) => {
       const gamesElement = document.createElement("div");
-      gamesElement.innerHTML = `<strong>${game.nome}</strong><p>${game.preco}</p>`;
+      gamesElement.innerHTML = 
+        `<div class="col">
+          <div class="elemento"><img class="cardimg" src="${jogo.img}" alt="${jogo.nome}">           
+              <h4 class="cardtitle">
+                ${jogo.nome}
+              </h4>
+              <p class="cardtext">R$ ${jogo.preco}</p></div>                      
+        </div>`;
       dataContainer.appendChild(gamesElement);
     });
   });
@@ -43,7 +50,7 @@ document.getElementById('btnDelete').addEventListener('click', () => {
 document.getElementById('btnUpdate').addEventListener('click', () => {
   const jogo = {
       nome: "The Legend of Zelda: Tears of the Kingdom",
-      img: "https://codetheworld.io/wp-content/uploads/2023/12/Dark-Souls.png",
+      img: "https://zelda.nintendo.com/tears-of-the-kingdom/_images/game/logo-shadow.png",
       preco: 300,
       id: 3
   }
